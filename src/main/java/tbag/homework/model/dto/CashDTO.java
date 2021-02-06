fb6cb9675e8fd4a8e26195e63db7fa05f74208f7
Gabor Tiba <tibag7@gmail.com>
2021-02-06 10:59:22 +0100

    implement deposit feature

--- src/main/java/tbag/homework/model/dto/CashDTO.java
+++ src/main/java/tbag/homework/model/dto/CashDTO.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CashDTO {
 
-    int userId;
     int accountId;
     double amount;
 
--- src/main/java/tbag/homework/service/TransactionService.java
+++ src/main/java/tbag/homework/service/TransactionService.java
@@ -5,12 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tbag.homework.model.Account;
+import tbag.homework.model.History;
 import tbag.homework.model.User;
 import tbag.homework.model.dto.CashDTO;
+import tbag.homework.model.enums.HistoryType;
+import tbag.homework.model.enums.TransactionType;
+import tbag.homework.repository.AccountRepository;
+import tbag.homework.repository.HistoryRepository;
 import tbag.homework.repository.UserRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -23,9 +30,28 @@ public class TransactionService {
 
     private final UserRepository userRepository;
 
+    private final AccountRepository accountRepository;
+
+    private final HistoryRepository historyRepository;
+
+    @Transactional
     public double deposit(CashDTO cashDTO) {
-        User user = userRepository.findById(cashDTO.getUserId()).orElseThrow();
-//        Account account =
+        Account account = accountRepository.findById(cashDTO.getAccountId()).orElseThrow();
+
+        account.setBalance(account.getBalance() + cashDTO.getAmount());
+        History history = History.builder()
+                .amount(cashDTO.getAmount())
+                .date(LocalDateTime.now())
+                .historyType(HistoryType.DEPOSIT)
+                .transactionType(TransactionType.EXTERNAL)
+                .issuerAccount(account)
+                .beneficiaryAccount(account)
+                .build();
+
+        accountRepository.saveAndFlush(account);
+        historyRepository.saveAndFlush(history);
+
+        return account.getBalance();
     }
 
 }

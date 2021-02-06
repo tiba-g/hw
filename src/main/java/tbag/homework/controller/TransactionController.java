42e398bfe6476b61d0634acbec0c5b745b7bd5e8
Gabor Tiba <tibag7@gmail.com>
2021-02-06 10:45:56 +0100

    Add repositories and basic exception handling

--- src/main/java/tbag/homework/controller/TransactionController.java
+++ src/main/java/tbag/homework/controller/TransactionController.java
@@ -3,7 +3,13 @@ package tbag.homework.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import tbag.homework.model.dto.CashDTO;
+import tbag.homework.service.TransactionService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestController
@@ -11,9 +17,17 @@ import tbag.homework.model.dto.CashDTO;
 @RequiredArgsConstructor
 public class TransactionController {
 
+    private final TransactionService transactionService;
+
     @PostMapping(value = "/deposit")
     public double deposit(@RequestBody CashDTO cashDTO)  {
-        return -21;
+        return transactionService.deposit(cashDTO);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public void onNoSuchElementException(NoSuchElementException e) {
+        log.error("Object does not exist. Message: {}, Exception: {}", e.getMessage(), e);
+        throw e;
     }
 
 }
---
+++ src/main/java/tbag/homework/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package tbag.homework.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tbag.homework.model.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+}
